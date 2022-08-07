341f9389016a10c2d0b6ac53e7cf62ad2dfecfb5
Vtav86 <Vatche_tavitian@hotmail.com>
2022-08-07 13:09:20 +0400

    Aug7

---
+++ CLFinalProject/gym_app/weather_info_function.py
@@ -0,0 +1,44 @@
+import requests
+
+conditions = {
+    'Thunderstorm': '⛈',
+    'Clouds': '☁',
+    'Drizzle': '🌧️',
+    'Rain': '🌧️',
+    'Snow': '❄',
+    'Mist': '🌫️',
+    'Smoke': '🌫️',
+    'Haze': '🌫️',
+    'Dust': '🌫️',
+    'Fog': '🌫️',
+    'Sand': '🌫️',
+    'Ash': '🌫️',
+    'Squall': '🌬️',
+    'Tornado': '🌪️',
+    'Clear': '☀️',
+    'Not found': 'A good day to climb!️',
+
+}
+
+
+def get_weather():
+    api_key = "563e77eb1aa4156bb5dcab597d8e1abd"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    city_name = "Yerevan"
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name + "&units=metric"
+    response = requests.get(complete_url)
+    x = response.json()
+    if x["cod"] != "404":
+        main_info = x['main']
+        current_temperature = main_info["temp"]
+        description = x["weather"]
+        weather_description = description[0]["main"]
+        try:
+            weather_image = conditions[weather_description]
+        except KeyError:
+            weather_image = conditions['Not found']
+        weather_statement = "Currently " + str(current_temperature) + "°C in Yerevan and " + weather_description + ' ' + weather_image
+        return weather_statement
+    else:
+        weather_statement = "It's a great day to climb!"
+        return weather_statement
